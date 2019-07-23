69c6f617f531dfee11991f4f16cd4bf0c07e6e13
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2019-07-23 00:11:53 -0400

    Webpack apparently

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'lib'),
+    library: 'dolphengine',
+    libraryTarget: 'umd'
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)?$/,
+        loader: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [/node_modules/]
+      }
+    ]
+  },
+
+  devServer: {
+    open: true
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+};
